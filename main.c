12b45aa4e0f221d93fbc097965450924da803f13
Aditya Pandya <68490046+pandya-aditya@users.noreply.github.com>
2023-11-14 21:49:26 -0500

    Buzzer Test

--- main.c
+++ main.c
@@ -5,6 +5,20 @@
 void SystemClock_Config(void);
 void MX_GPIO_Init(void);
 
+void BuzzerPlayNote(int prescalerfornote, int NoteDurationMs)
+{
+	HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_2);
+
+		for(int i = 0; i<NoteDurationMs ; i++)
+		{
+			TIM2->PSC = prescalerfornote; //change prescaler for have the frequency of our note
+			HAL_Delay(1);
+		}
+		TIM2->PSC=0;
+	HAL_TIM_PWM_Stop(&htim2,TIM_CHANNEL_2);
+
+}
+
 int main(void)
 {
 
@@ -14,7 +28,8 @@ int main(void)
   MX_GPIO_Init();
 
   while (1)	{
-
+    Buzzer_Play_Note(142, 5);
+    HAL_Delay(3600000);
   }
 
 }
